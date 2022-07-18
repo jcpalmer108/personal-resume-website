90c87834a4d488d5bb4c5831c42798f27e1c9486
Jenna Palmer <jenna.palmer@alaskaair.com>
2022-07-17 18:42:29 -0700

    added styles to url links

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,18 +2,21 @@ import React, { useEffect, useState } from "react";
 import Section from "./Section";
 import { 
   Image, 
-  Menu, 
+  TopNav, 
   TopBar, 
   menuBreakpoint, 
+  SocialsWrapper,
   MobileMenu,
+  Pages,
+  PagesWrapper,
   Socials,
   Icon
-} from "@/styles/components/Navbar";
+} from "../styles/components/Navbar";
 import logo from "../assets/images/jennapalmer-dark.svg";
 import { redstone } from "@/styles/variables";
 import { Turn as Hamburger } from 'hamburger-react';
 import socials from '../assets/content/socials.json';
-import { Link, NavbarProps } from '../types/Navbar'
+import { Link, NavbarProps } from '../types/Navbar';
 
 export default function Navbar({ menu }: NavbarProps) {
   const [isOpen, setIsOpen] = useState(true)
@@ -42,7 +45,7 @@ export default function Navbar({ menu }: NavbarProps) {
     <Section wide light>
       <TopBar>
         <Image src={logo} alt="logo" />
-        <Menu>
+        <TopNav>
           {
             isDesktop ? (
               <div>blah</div>
@@ -56,14 +59,21 @@ export default function Navbar({ menu }: NavbarProps) {
               />
             )
           }
-        </Menu>
+        </TopNav>
       </TopBar>
       <MobileMenu>
-        {socials.links && socials.links.map((link: Link) => (
-          <Socials href={link.url} key={link.key}>
-            <Icon src={require("../assets/images/" + link.key + ".svg")} alt={link.key} />
-          </Socials>
-        ))}
+        <PagesWrapper>
+          {menu.map((link) => (
+            <Pages href={`#${link.key}`}>{link.label}</Pages>
+          ))}
+        </PagesWrapper>
+        <SocialsWrapper>
+          {socials.links && socials.links.map((link: Link) => (
+            <Socials href={link.url} key={link.key}>
+              <Icon src={require("../assets/images/" + link.key + ".svg")} alt={link.key} />
+            </Socials>
+          ))}
+        </SocialsWrapper>
       </MobileMenu>
     </Section>
   )
--- src/styles/components/Navbar.ts
+++ src/styles/components/Navbar.ts
@@ -14,7 +14,7 @@ export const Image = styled.img`
 
 `
 
-export const Menu = styled.div`
+export const TopNav = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
@@ -36,40 +36,64 @@ export const Menu = styled.div`
   `
 
 export const TopBar = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-  padding: 30px 20px 20px 20px;
+display: flex;
+flex-direction: row;
+align-items: center;
+justify-content: space-between;
+padding: 30px 20px 20px 20px;
+transition: all ease 1s;
+
+@media (min-width: ${menuBreakpoint}px) {
+  padding: 20px 60px;
   transition: all ease 1s;
+}
 
-  @media (min-width: ${menuBreakpoint}px) {
-    padding: 20px 60px;
-    transition: all ease 1s;
-  }
+`
 
-  `
+export const MobileMenu = styled.div`
+  min-height: 320px;
+  height: calc(100vh - 95px);
+  display: flex;
+  flex-direction: column;
+`
 
-  export const MobileMenu = styled.div`
-    background: white;
-    justify-content: space-around;
-    display: flex;
-    height: 60px;
-  `
+export const PagesWrapper = styled.div`
+  flex-grow: 1;
+  display: flex;
+  padding: 40px 0px;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
 
-  export const Socials = styled.a`
-    border-left: 1px solid ${silver_polish};
-    border-bottom: 1px solid ${silver_polish};
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+export const Pages = styled.a`
+  height: 100%;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  color: black;
+`
 
-    &:first-child {
-      border-left: none;
-   }
-  `
+export const SocialsWrapper = styled.div`
+  background: white;
+  justify-content: space-around;
+  display: flex;
+  height: 60px;
+`
 
-  export const Icon = styled.img`
+export const Socials = styled.a`
+  border-left: 1px solid ${silver_polish};
+  border-bottom: 1px solid ${silver_polish};
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &:first-child {
+    border-left: none;
+}
+`
+
+export const Icon = styled.img`
   height: 26px;
-  `
+`
