384ecb4d30d5d403a633d866118c19708e1c40ac
Jenna Palmer <jenna.palmer@alaskaair.com>
2022-07-23 03:49:11 -0700

    added logic to update selected index

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -8,27 +8,59 @@ import {
   ArrowLeft, 
   ArrowRight, 
   Arrows, 
-  Content 
+  Content,
+  Circle,
+  Option,
+  Dot
 } from "../styles/components/Testimonials"
 import Section from "./Section"
+import arrowLeft from "../assets/images/arrow-left-pink.svg"
+import arrowRight from "../assets/images/arrow-right-pink.svg"
+import { useState } from "react"
 
 export default function Testimonials({ content }: TestimonialProps) {
+  let [ selectedIndex, setSelectedIndex ] = useState(0)
+  const quotes = content?.subSection?.quotes
+
+  function increment () {
+    console.log('hit1')
+    return quotes && selectedIndex >= (quotes.length - 1) ? 
+      setSelectedIndex(0) : 
+      setSelectedIndex(selectedIndex + 1);
+  }
+
+  function decrement () {
+    console.log('hit2')
+    return quotes && selectedIndex <= 0 ? 
+    setSelectedIndex(quotes.length - 1) : 
+    setSelectedIndex(selectedIndex - 1);
+  }
   
   return (
     <Section>
       <Wrapper>
-        <ArrowLeft />
+        <ArrowLeft onClick={decrement} src={arrowLeft} alt="arrow left" desktop />
         <Content>
           <h2>Testimonials</h2>
-          <Quote>Test</Quote>
-          <Person>blah</Person>
-          <Title>blah</Title>
-          <Dots>test</Dots>
+          <Quote>{quotes && quotes[selectedIndex].quote}</Quote>
+          <Person>{quotes && quotes[selectedIndex].person}</Person>
+          <Title>{quotes && quotes[selectedIndex].title}</Title>
+          <Dots>
+            {quotes && quotes.map((item, index) => {
+              return (<Dot selected={selectedIndex === index}/>)
+            })}
+          </Dots>
         </Content>
-        <ArrowRight />
+        <ArrowRight onClick={increment} src={arrowRight} alt="arrow right" desktop />
         <Arrows>
-          <ArrowLeft />
-          <ArrowRight />
+          <Option onClick={decrement}>
+            <Circle />
+            <ArrowLeft src={arrowLeft} alt="arrow left" />
+          </Option>
+          <Option onClick={increment}>
+            <Circle />
+            <ArrowRight src={arrowRight} alt="arrow right"/>
+          </Option>
         </Arrows>
       </Wrapper>
     </Section>
--- src/styles/components/Testimonials.ts
+++ src/styles/components/Testimonials.ts
@@ -1,7 +1,9 @@
 import styled from "styled-components"
+import { redstone } from "../variables"
 
 export const Wrapper = styled.div`
-
+  display: flex;
+  flex-direction: column;
 `
 
 export const Content = styled.div`
@@ -9,29 +11,66 @@ export const Content = styled.div`
 `
 
 export const Quote = styled.p`
+  font-size: 15px;
+  font-family: Gilroy-Regular;
 
 `
 
 export const Person = styled.div`
+  font-size: 16;
+  font-family: Gilroy-Medium;
 
 `
 
 export const Title = styled.div`
+  font-size: 12px;
+  font-family: Gilroy-Regular;
 
 `
 
 export const Dots = styled.div`
-
+  display: flex;
+  
 `
+
+type DotProps = {
+  selected: boolean
+}
+
+export const Dot = styled.div<DotProps>`
+  height: 5px;
+  width: 5px;
+  background: ${props => props.selected ? 'blue' : 'red'};
+  border-radius: 50%;
+`
+
 
 export const Arrows = styled.div`
 
 `
 
-export const ArrowLeft = styled.div`
+type ArrowProps = {
+  desktop?: boolean | undefined
+}
 
+export const ArrowLeft = styled.img<ArrowProps>`
+  display: ${props => props.desktop ? 'none' : 'block'}; 
+  width: 19px;
 `
 
-export const ArrowRight = styled.div`
+export const ArrowRight = styled.img<ArrowProps>`
+  display: ${props => props.desktop ? 'none' : 'block'};
+  width: 19px;
+`
+
+export const Circle = styled.div`
+  background: ${redstone};
+  opacity: 10%;
+  height: 40px;
+  width: 40px;
+  border-radius: 50%;
+
+`
+export const Option = styled.div`
 
 `
