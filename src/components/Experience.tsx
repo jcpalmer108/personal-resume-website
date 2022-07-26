875b867ba384d99d681828d13b553f9f223317d5
Jenna Palmer <jenna.palmer@alaskaair.com>
2022-07-25 22:44:20 -0700

    added some foundational css logic for mobile, mainly upper section

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -3,8 +3,8 @@ import { Wrapper } from "../styles/components/Experience"
 import {
   Upper,
   Title,
-  Description,
-  Paragraph,
+  PageDescription,
+  PageParagraph,
   Tile,
   Icon,
   Label,
@@ -14,27 +14,52 @@ import {
   Content,
   JobTitle,
   Info,
-  DiagonalLine
+  DiagonalLine,
+  JobDescription,
+  JobParagraph
 } from "../styles/components/Experience"
 import callMe from "../assets/images/call-me.svg"
 import emailMe from "../assets/images/email-me.svg"
 import diagonal from "../assets/images/diagonal-line.svg"
 import Section from "./Section"
 
+// TODO: remove contact info to content.json
+// email, phone
+
+// adjust page paragraph padding
+// adjust job description paragraph padding
+// make sure icons, title, and info have all the right spacing
+// format mobile tile buttons
+
+// TABLET & DESKTOP
+// upper
+//   title
+//   page description
+//   tile
+// lower
+//   wrapper
+//   job items
+//     title
+//     info, 
+//     description, 
+//     logo, 
+//     diagonal,
+//     line
+
 export default function Experience({ content }: ExperienceProps) {
   return (
-    <Section label={content?.label}>
+    <Section center label={content?.label}>
       <Wrapper>
         <Upper>
           <Title>{content?.title}</Title>
-          <Description>
-            { content?.description && content?.description.map((paragraph, index) => <Paragraph key={`Paragraph ${index + 1}`}>{paragraph}</Paragraph>)}
-          </Description>
-          <Tile>
+          <PageDescription>
+            { content?.description && content?.description.map((paragraph, index) => <PageParagraph key={`PageParagraph ${index + 1}`}>{paragraph}</PageParagraph>)}
+          </PageDescription>
+          <Tile href="tel:3142212451" desktop>
             <Icon src={callMe} alt="call me" />
             <Label>CALL ME</Label>
           </Tile>
-          <Tile>
+          <Tile href="mailto:jenna@jennapalmer.info" desktop>
             <Icon src={emailMe} alt="email me"/>
             <Label>EMAIL ME</Label>
           </Tile>
@@ -49,12 +74,20 @@ export default function Experience({ content }: ExperienceProps) {
                   <Info>{job.employer} / {job.timeline.start}-{job.timeline.end}</Info>
                 </Content>
                 <DiagonalLine src={diagonal} alt="forward slash" />
-                <Description>
-                  {content.description && content?.description.map((paragraph, index) => <Paragraph key={`Paragraph ${index + 1}`}>{paragraph}</Paragraph>)}
-                </Description>
+                <JobDescription>
+                  {content.description && content?.description.map((paragraph, index) => <JobParagraph key={`PageParagraph ${index + 1}`}>{paragraph}</JobParagraph>)}
+                </JobDescription>
               </Job>
             ))
           }
+          <Tile href="tel:3142212451">
+            <Icon src={callMe} alt="call me" />
+            <Label>CALL ME</Label>
+          </Tile>
+          <Tile href="mailto:jenna@jennapalmer.info">
+            <Icon src={emailMe} alt="email me"/>
+            <Label>EMAIL ME</Label>
+          </Tile>
         </Lower>
       </Wrapper>
     </Section>
--- src/styles/components/Experience.ts
+++ src/styles/components/Experience.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components"
 
 export const Wrapper = styled.div`
-
+  text-align: center;
 `
 
 export const Upper = styled.div`
@@ -9,19 +9,27 @@ export const Upper = styled.div`
 `
 
 export const Title = styled.h2`
+padding-bottom: 14px;
 
 `
 
-export const Description = styled.div`
+export const PageDescription = styled.div`
+  font-family: Gilroy-Regular;
 
 `
 
-export const Paragraph = styled.p`
+export const PageParagraph = styled.p`
+  font-size: 16px;
+  line-spacing: 26px;
+
 
 `
 
-export const Tile = styled.div`
-
+type TileProps = {
+  desktop?: boolean | undefined
+}
+export const Tile = styled.a<TileProps>`
+  display: ${props => props.desktop ? 'none' : 'block'};
 `
 
 export const Icon = styled.img`
@@ -37,25 +45,54 @@ export const Lower = styled.div`
 `
 
 export const Job = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-top: 40px;
+
+  &:first-child {
+    padding-top: 14px;
+  }
 
 `
 
 export const Logo = styled.img`
-
+  padding: 20px;
+  width: 100px;
+  height: 100px;
+  margin: auto;
 `
 
 export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
 
 `
 
-export const JobTitle = styled.h3`
-
+export const JobTitle = styled.div`
+  font-family: Butler-Regular;
+  font-size: 25px;
+  line-spacing: 65px;
 `
 
 export const Info = styled.span`
+  font-family: Gilroy-Regular;
+  font-style: italic;
+  font-size: 14px;
+  padding: 12px;
+`
+
+export const JobDescription = styled.div`
+  font-family: Gilroy-Regular;
+
+`
+
+export const JobParagraph = styled.p`
+  font-size: 16px;
+  line-spacing: 26px;
+
 
 `
 
 export const DiagonalLine = styled.img`
-
+  display: none;
 `
