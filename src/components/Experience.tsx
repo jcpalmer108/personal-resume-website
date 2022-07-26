16c66db96c758baed880064808dc03aa0442e1bc
Jenna Palmer <jenna.palmer@alaskaair.com>
2022-07-25 19:39:50 -0700

    added base layout and images

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,11 +1,61 @@
 import { ExperienceProps } from "../types/Experience"
 import { Wrapper } from "../styles/components/Experience"
+import {
+  Upper,
+  Title,
+  Description,
+  Paragraph,
+  Tile,
+  Icon,
+  Label,
+  Lower,
+  Job,
+  Logo,
+  Content,
+  JobTitle,
+  Info,
+  DiagonalLine
+} from "../styles/components/Experience"
+import alaska from "../assets/images/alaska.svg";
 import Section from "./Section"
 
 export default function Experience({ content }: ExperienceProps) {
   return (
     <Section label={content?.label}>
-      <Wrapper>blah blha </Wrapper>
+      <Wrapper>
+        <Upper>
+          <Title>{content?.title}</Title>
+          <Description>
+            { content?.description && content?.description.map((paragraph) => <Paragraph>{paragraph}</Paragraph>)}
+          </Description>
+          <Tile>
+            <Icon src="../assets/images/call-me.svg" />
+            <Label>CALL ME</Label>
+          </Tile>
+          <Tile>
+            <Icon src="../assets/images/email-me.svg" />
+            <Label>EMAIL ME</Label>
+          </Tile>
+        </Upper>
+        <Lower>
+          {
+            content?.subSection?.experience && content?.subSection?.experience.map((job) => (
+              <Job>
+                <Logo src={require("../assets/images/" + job.icon + ".svg")} alt={job.icon} />
+                <Content>
+                  <JobTitle>{job.title}</JobTitle>
+                  <Info>{job.employer} / {job.timeline.start}-{job.timeline.end}</Info>
+                </Content>
+                <DiagonalLine src="../assets/images/diagonal-line.svg" alt="forward slash" />
+                <Description>
+                  {content.description && content?.description.map((paragraph) => <Paragraph>{paragraph}</Paragraph>)}
+                </Description>
+              </Job>
+            ))
+          }
+        </Lower>
+        <img src={alaska} alt="alaska" />
+      </Wrapper>
     </Section>
   )
 }
--- src/styles/components/Experience.ts
+++ src/styles/components/Experience.ts
@@ -3,3 +3,59 @@ import styled from "styled-components"
 export const Wrapper = styled.div`
 
 `
+
+export const Upper = styled.div`
+
+`
+
+export const Title = styled.h2`
+
+`
+
+export const Description = styled.div`
+
+`
+
+export const Paragraph = styled.p`
+
+`
+
+export const Tile = styled.img`
+
+`
+
+export const Icon = styled.img`
+
+`
+
+export const Label = styled.span`
+
+`
+
+export const Lower = styled.div`
+
+`
+
+export const Job = styled.div`
+
+`
+
+export const Logo = styled.img`
+
+`
+
+export const Content = styled.div`
+
+`
+
+export const JobTitle = styled.h3`
+
+`
+
+export const Info = styled.span`
+
+`
+
+export const DiagonalLine = styled.img`
+
+`
