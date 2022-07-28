96d1dfc5f55a6421527133623a66eb624467fc68
Jenna Palmer <jenna.palmer@alaskaair.com>
2022-07-27 23:45:14 -0700

    wrote tests for skills

--- src/__tests__/components/About.test.tsx
+++ src/__tests__/components/About.test.tsx
@@ -1,12 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import 'jest-styled-components'
-import About from '../../components/About';
+import Skills from '../../components/Skills';
+import Button from "../../components/Button";
 
 jest.mock('../../components/Button')
 
 const params = {
   key: "about",
-  label: "About",
+  label: "Skills",
   inMenu: true,
   title: "test title",
   description: [
@@ -14,75 +15,46 @@ const params = {
     "test contents 2"
   ],
   subSection: {
-    education: [
+    skills: [
       {
-        key: "webster",
-        school: "Webster University",
-        degree: "Bachelors of Science in Computer Science",
-        timeline: {
-          start: 2014,
-          end: 2017
-        }
+        key: "testskill1",
+        label: "Test Skill 1",
+        url: "https://test.skill.one/"
       },
       {
-        key: "harvard",
-        school: "Harvard University",
-        degree: "Bachelors of Smartiness in Computer Science",
-        timeline: {
-          start: 2027,
-          end: 2030
-        }
-      }
+        key: "testskill2",
+        label: "Test Skill 2",
+        url: "https://test.skill.two/"
+      },
     ]
   }
 }
 
-const mockSocials = [
-  {
-    key: "github",
-    label: "Github",
-    url: "https://www.github.com/jcpalmer108/"
-  },
-  {
-    key: "leetcode",
-    label: "Leetcode",
-    url: "https://leetcode.com/darthToaster/"
-  }
-]
-
-describe('About', () => {
+describe('Skills', () => {
   test('renders if only required params are passed in', () => {
     render(
-      <About content={params} />
+      <Skills content={params} />
     )
   
-    expect(screen.getByTestId('About')).toMatchSnapshot()
+    expect(screen.getByTestId('Skills')).toMatchSnapshot()
     expect(screen.getByTestId('Title')).toHaveTextContent(params.title)
 
     params.description.forEach((item, index) => {
       expect(screen.getByTestId(`Description ${index + 1}`)).toHaveTextContent(item)
     })
-    
-    expect(screen.getByTestId('Schooling').childNodes).toHaveLength(params.subSection.education.length + 1)
-    params.subSection.education.forEach((item, index) => {
-      expect(screen.getByTestId(`School ${index + 1}`).childNodes[0]).toHaveTextContent(item.school)
-      expect(screen.getByTestId(`School ${index + 1}`).childNodes[1]).toHaveTextContent(item.degree + ' - ' + item.timeline.start + '-' + item.timeline.end)
+
+    expect(screen.getByTestId('Row 1').childNodes).toHaveLength(3)
+    expect(screen.getByTestId('Row 2').childNodes).toHaveLength(5)
+    expect(screen.getByTestId('Row 3').childNodes).toHaveLength(3)
+    expect(Button).toHaveBeenCalled()
+
+    params.subSection.skills.forEach((item, index) => {
+      expect(screen.getByRole('link', { name: item.label })).toBeTruthy()
+      expect(screen.getByRole('img', { name: item.label })).toHaveAttribute('href', item.url)
+      expect(screen.getByRole('img', { name: item.label })).toHaveAttribute('alt', item.label)
+
     })
 
-    expect(screen.getByTestId('Connect')).toMatchSnapshot()
-    mockSocials.forEach((link) => {
-      expect(screen.getByTestId('Connect')).toHaveTextContent(link.label)
-      expect(screen.getByRole('link', { name: link.label })).toBeTruthy()
-      expect(screen.getByRole('link', { name: link.label })).toHaveAttribute(
-        'href',
-        link.url
-      )
-    })
-
-    expect(screen.getByTestId('Start a Project')).toMatchSnapshot()
-    expect(screen.getByRole('link', { name: 'START A PROJECT' })).toHaveAttribute(
-      'href',
-      '#contact'
-    )
+    expect(screen.getByTestId('Marquee')).toBeTruthy()
   })
 })
---
+++ src/__tests__/components/Skills.test.tsx
@@ -0,0 +1,88 @@
+import { render, screen } from '@testing-library/react';
+import 'jest-styled-components'
+import Skills from '../../components/Skills';
+
+jest.mock('../../components/Button')
+
+const params = {
+  key: "about",
+  label: "Skills",
+  inMenu: true,
+  title: "test title",
+  description: [
+    "test contents 1",
+    "test contents 2"
+  ],
+  subSection: {
+    education: [
+      {
+        key: "webster",
+        school: "Webster University",
+        degree: "Bachelors of Science in Computer Science",
+        timeline: {
+          start: 2014,
+          end: 2017
+        }
+      },
+      {
+        key: "harvard",
+        school: "Harvard University",
+        degree: "Bachelors of Smartiness in Computer Science",
+        timeline: {
+          start: 2027,
+          end: 2030
+        }
+      }
+    ]
+  }
+}
+
+const mockSocials = [
+  {
+    key: "github",
+    label: "Github",
+    url: "https://www.github.com/jcpalmer108/"
+  },
+  {
+    key: "leetcode",
+    label: "Leetcode",
+    url: "https://leetcode.com/darthToaster/"
+  }
+]
+
+describe('Skills', () => {
+  test('renders if only required params are passed in', () => {
+    render(
+      <Skills content={params} />
+    )
+  
+    expect(screen.getByTestId('Skills')).toMatchSnapshot()
+    expect(screen.getByTestId('Title')).toHaveTextContent(params.title)
+
+    params.description.forEach((item, index) => {
+      expect(screen.getByTestId(`Description ${index + 1}`)).toHaveTextContent(item)
+    })
+    
+    expect(screen.getByTestId('Schooling').childNodes).toHaveLength(params.subSection.education.length + 1)
+    params.subSection.education.forEach((item, index) => {
+      expect(screen.getByTestId(`School ${index + 1}`).childNodes[0]).toHaveTextContent(item.school)
+      expect(screen.getByTestId(`School ${index + 1}`).childNodes[1]).toHaveTextContent(item.degree + ' - ' + item.timeline.start + '-' + item.timeline.end)
+    })
+
+    expect(screen.getByTestId('Connect')).toMatchSnapshot()
+    mockSocials.forEach((link) => {
+      expect(screen.getByTestId('Connect')).toHaveTextContent(link.label)
+      expect(screen.getByRole('link', { name: link.label })).toBeTruthy()
+      expect(screen.getByRole('link', { name: link.label })).toHaveAttribute(
+        'href',
+        link.url
+      )
+    })
+
+    expect(screen.getByTestId('Start a Project')).toMatchSnapshot()
+    expect(screen.getByRole('link', { name: 'START A PROJECT' })).toHaveAttribute(
+      'href',
+      '#contact'
+    )
+  })
+})
