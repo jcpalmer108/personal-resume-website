c4d534614454a2cb52b0bb76fac3640337254005
Jenna Palmer <jenna.palmer@alaskaair.com>
2022-08-31 00:23:51 -0700

    removed router logic

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Routes from "./Routes";
+import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
-root.render(
-  <Router basename={process.env.PUBLIC_URL}>
-    <Routes />
-  </Router>
-);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
