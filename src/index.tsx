a64965c58bd2867cdcc5b87412fe983bf994708d
Jenna Palmer <jenna.palmer@alaskaair.com>
2022-08-30 23:54:33 -0700

    added router logic

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
-  // <React.StrictMode>
-  <App />
-  // </React.StrictMode>
+  <Router basename={process.env.PUBLIC_URL}>
+    <App />
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
