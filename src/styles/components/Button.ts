5bfe53c140879601dcea30db04e775f9ae0acd79
Jenna Palmer <jenna.palmer@alaskaair.com>
2022-09-02 14:46:06 -0700

    updated button styles to have animations

--- src/styles/components/Button.ts
+++ src/styles/components/Button.ts
@@ -14,11 +14,28 @@ export const Link = styled.a`
   color: white;
   font-family: Gilroy-Medium;
   font-size: 12px;
+  transition: all 500ms ease;
 
   @media (min-width: ${tabletBreakpoint}px) {
     width: 214px;
     height: 60px;
     font-size: 14px;
+    border: 1px solid transparent;
+    transition: all 500ms ease;
+
+    &:hover {
+      color: ${redstone};
+      background: white;
+      transition: all 500ms ease;
+      font-size: 16px;
+      border: ${redstone} 1px solid;
+
+      img {
+        filter: invert(59%) sepia(50%) saturate(1158%) hue-rotate(312deg)
+          brightness(92%) contrast(95%);
+        transition: all 500ms ease;
+      }
+    }
   }
 
   @media (min-width: ${desktopBreakpoint}px) {
@@ -29,4 +46,5 @@ export const Link = styled.a`
 
 export const ArrowRight = styled.img`
   width: 22px;
+  padding-bottom: 3px;
 `;
